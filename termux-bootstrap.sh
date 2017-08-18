9345bc4bcca388428b5541a509c1fa6022c62dc7
Adam Miller <maxamillion@fedoraproject.org>
2017-08-18 00:36:23 -0500

    add some utils: strace, ltrace, nmap, tracepath
    Signed-off-by: Adam Miller <maxamillion@fedoraproject.org>

--- termux-bootstrap.sh
+++ termux-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 apt update
-apt -y install vim-python python python2 ruby clang golang git htop fontconfig fontconfig-util tmux openssh proot
+apt -y install vim-python python python2 ruby clang golang git htop fontconfig fontconfig-util tmux openssh proot strace ltrace tracepath nmap
 
 termux-storage-setup
 
