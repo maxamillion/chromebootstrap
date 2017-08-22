723c906e45f53a947a6527e9d4e7d04f3412c77a
Adam Miller <maxamillion@fedoraproject.org>
2017-08-21 22:41:48 -0500

    use dotfiles bootstrap scripts
    Signed-off-by: Adam Miller <maxamillion@fedoraproject.org>

--- termux-bootstrap.sh
+++ termux-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 apt update
-apt -y install vim-python python python2 ruby clang golang git htop fontconfig fontconfig-util tmux openssh proot strace ltrace tracepath nmap
+apt -y install vim-python python python2 ruby clang golang git htop fontconfig fontconfig-utils tmux openssh proot strace ltrace tracepath nmap
 
 pip install virtualenv
 pip install ptpython
@@ -11,48 +11,22 @@ termux-storage-setup
 cd ~/
 git clone https://github.com/maxamillion/dotfiles
 
-# Ensure the needed dirs exist
-mkdir -p ~/.config/{dunst,i3,i3status,fontconfig}
-mkdir -p ~/.config/fontconfig/conf.d
-mkdir ~/.tmuxinator
-mkdir ~/.ptpython
-mkdir ~/.fonts
+# Nuke old dirs and resetup (this is apparently needed because Termux does
+# something weird)
+#
+rm -fr ~/.config/{dunst,i3,i3status,fontconfig}
+rm -fr ~/.config/fontconfig/conf.d
+rm -fr ~/.tmuxinator
+rm -fr ~/.ptpython
+rm -fr ~/.fonts
+rm -fr ~/.vim*
+rm -fr ~/vimified
 
-# Symlink the conf files
-ln -s ~/dotfiles/dunstrc ~/.config/dunst/dunstrc
-ln -s ~/dotfiles/i3-config ~/.config/i3/config
-ln -s ~/dotfiles/i3status-config ~/.config/i3status/config
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/tmuxinator-wm.yml ~/.tmuxinator/wm.yml
-ln -s ~/dotfiles/screenrc ~/.screenrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/inputrc ~/.inputrc
-ln -s ~/dotfiles/ptpython_config.py ~/.ptpython/config.py
-ln -s ~/dotfiles/bashrc ~/.bashrc
+bash ~/dotfiles/bootstrap.sh
 
 cd ~/
 
-git clone git://github.com/zaiste/vimified.git
-ln -sfn vimified/ ~/.vim
-ln -sfn vimified/vimrc ~/.vimrc
-cd ~/vimified
-mkdir bundle
-mkdir -p tmp/backup tmp/swap tmp/undo
-git clone https://github.com/gmarik/vundle.git bundle/vundle
-ln -s ~/dotfiles/local.vimrc ~/vimified/local.vimrc
-ln -s ~/dotfiles/after.vimrc ~/vimified/after.vimrc
-vim +BundleInstall +qall
+bash ~/dotfiles/bootstrap-vim.sh
 
 cd ~/
 
-fontdir=$(mktemp -d)
-pushd $fontdir
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts/
-mkdir -p ~/.config/fontconfig/conf.d/
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-popd
-
-rm -fr ${fontdir}
