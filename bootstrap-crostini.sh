ca14e3890be26a87dd0171d506106b25a8b53c98
Adam Miller <admiller@redhat.com>
2018-09-26 11:54:18 -0500

    add docker setup
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-crostini.sh
+++ bootstrap-crostini.sh
@@ -47,6 +47,20 @@ pip install --user pdbpp
 pip install --user ptpython
 pip install --user pipenv
 
+# Setup docker, becuase $reasons
+# https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-repository
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+
 # Setup my dotfiles
 cd ~/
 git clone https://github.com/maxamillion/dotfiles
