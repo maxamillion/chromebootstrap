f3dbe25510f9035a4d479f65360ceae5747ee4da
Adam Miller <admiller@redhat.com>
2018-10-31 16:38:50 +0000

    add keybase to crostini
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-crostini.sh
+++ bootstrap-crostini.sh
@@ -93,6 +93,20 @@ docker run hello-world
 # END DOCKER
 ################################################################################
 
+################################################################################
+# START KEYBASE
+
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+# if you see an error about missing `libappindicator1`
+# from the next command, you can ignore it, as the
+# subsequent command corrects it
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f
+#run_keybase
+
+# END KEYBASE
+################################################################################
+
 # Setup my dotfiles
 cd ~/
 git clone https://github.com/maxamillion/dotfiles
