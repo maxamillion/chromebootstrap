26249c7dbcee3905bfd74845b0111e96fd8141ad
Adam Miller <admiller@redhat.com>
2018-11-29 09:59:46 -0600

    update docker in crostini craziness
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-crostini.sh
+++ bootstrap-crostini.sh
@@ -53,25 +53,18 @@ pip install --user molecule
 # BEGIN DOCKER
 #
 # Setup docker, becuase $reasons
+
+# Docker syscall blacklist workaround for crostini because we can't have nice things
+#
+# crosh> vmc start termina
+# (termina) chronos@localhost ~ $ lxc profile unset default security.syscalls.blacklist
+# (termina) chronos@localhost ~ $ lxc profile apply penguin default
+# Profiles default applied to penguin
+# (termina) chronos@localhost ~ $ lxc restart penguin
+
 ##
 ## Once docker goes stable with the fixes in, switch to using the official repo
 ## https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-repository
-sudo apt -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    btrfs-progs \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce
-
-
-# For now we have to install from this guide
-#   https://www.reddit.com/r/Crostini/comments/9jabhq/docker_now_working/
 #sudo apt -y install \
 #    apt-transport-https \
 #    ca-certificates \
@@ -84,10 +77,26 @@ sudo apt -y install docker-ce
 #    "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 #sudo apt update
 #sudo apt -y install docker-ce
+
+
+# For now we have to install from this random reddit post
+#   https://www.reddit.com/r/Crostini/comments/9jabhq/docker_now_working/
+#sudo apt -y install \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    gnupg2 \
+#    btrfs-progs \
+#    software-properties-common
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+#sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+#sudo apt update
+#sudo apt install docker-ce
 #wget https://www.dropbox.com/s/332lj9d1zkp9t84/runc-chromeos?dl=1 -O /tmp/runc-chromeos
-#sudo mv /tmp/runc-chromeos /usr/local/sbin/
-#sudo chmod +x /usr/local/sbin/runc-chromeos
-#wget https://www.dropbox.com/s/us4zhl2v6rx3l08/daemon.json?dl=1 -O /tmp/daemon.json
+#sudo mv /tmp/runc-chromeos /usr/local/bin/
+#sudo chmod +x /usr/local/bin/runc-chromeos
+#wget https://www.dropbox.com/s/camzt33ueudubc1/daemon.json?dl=1 -O /tmp/daemon.json
 #sudo mv /tmp/daemon.json /etc/docker/
 #sudo service docker restart
 #sudo docker run hello-world
